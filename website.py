6ba508f3a63e986cbbb592e04b4cf9a68a46354d
Sare Naz Bayraktutan <116021150+sareersoy@users.noreply.github.com>
2024-05-27 22:40:06 +0300

    Update website.py

--- website.py
+++ website.py
@@ -90,7 +90,7 @@ if page == 'Homepage':
     pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="600" type="application/pdf">'
     st.markdown(pdf_display, unsafe_allow_html=True)
     st.markdown(
-        f'<a href="data:file/pdf;base64,{base64_pdf}" download="{"\Red and Gray Contemporary Editorial Landscape University Research Poster.pdf"}">Click here to download the poster</a>',
+        f'<a href="data:file/pdf;base64,{base64_pdf}" download="{"Red and Gray Contemporary Editorial Landscape University Research Poster.pdf"}">Click here to download the poster</a>',
         unsafe_allow_html=True
     )
 
